b3cf25218c401ae3d931a6fc8c86006080fc73ae
Thomas Berry <thomasdpb@gmail.com>
2019-03-26 22:41:17 +0000

    fix com o ze

--- spherical_wrist_sol.m
+++ spherical_wrist_sol.m
@@ -11,8 +11,10 @@ function [theta] = spherical_wrist_sol( rot )
     elseif sum(abs(rot(:,3)-[ 0 0 -1 ].'))<3*10e-5
         theta = zeros(2,3);
         disp('Here we have infinite solutions');
-        theta(1,1) = -atan2(rot(2,1),rot(1,1));
-        theta(2,3) = -atan2(rot(2,1),rot(1,1));
+        theta(1,1) = atan2(rot(2,1),rot(1,1));
+        theta(2,3) = atan2(rot(2,1),rot(1,1));
+        theta(1,2) = pi;
+        theta(2,2) = pi;
     else
         theta(1,1) = atan2(rot(2,3),rot(1,3));
         theta(1,2) = atan2(norm(rot(1:2,3)),rot(3,3));
